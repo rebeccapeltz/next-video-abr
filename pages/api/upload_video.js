9cbefad87443549786dc26ff33d4b3d7cf659d0e
rebeccapeltz <rebeccapeltz@gmail.com>
2022-07-26 16:36:43 -0700

    upload local video

--- pages/api/upload_video.js
+++ pages/api/upload_video.js
@@ -5,10 +5,12 @@ const cloudinary = require('cloudinary').v2
 export default async function handler(req, res) {
   const resp = await cloudinary.uploader
     // .upload('https://res.cloudinary.com/picturecloud7/video/upload/v1584394767/remote-media/video/rooster.mp4', {
-    //   .upload('./assets/rooster.mp4', {  
-    .upload('https://res.cloudinary.com/picturecloud7/video/upload/v1637431015/People_Surfing_cqmlmx.mp4', {
+    // //   .upload('./assets/rooster.mp4', {  
+    .upload('./assets/surf.mp4', {
 
-      public_id: 'surf-test',
+      // .upload('https://res.cloudinary.com/picturecloud7/video/upload/v1637431015/People_Surfing_cqmlmx.mp4', {
+
+      public_id: `surf-${Date.now()}`,
       resource_type: 'video',
       invalidate: true,
       overwrite: true
