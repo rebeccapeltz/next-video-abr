a8cc3da4ed54d07d6a059937708868773063cd0f
rebeccapeltz <rebeccapeltz@gmail.com>
2022-07-26 17:00:56 -0700

    pick up remote video

--- pages/api/upload_video.js
+++ pages/api/upload_video.js
@@ -6,9 +6,9 @@ export default async function handler(req, res) {
   const resp = await cloudinary.uploader
     // .upload('https://res.cloudinary.com/picturecloud7/video/upload/v1584394767/remote-media/video/rooster.mp4', {
     // //   .upload('./assets/rooster.mp4', {  
-    .upload('/assets/surf.mp4', {
+    // .upload('/assets/surf.mp4', {
 
-      // .upload('https://res.cloudinary.com/picturecloud7/video/upload/v1637431015/People_Surfing_cqmlmx.mp4', {
+    .upload('https://res.cloudinary.com/picturecloud7/video/upload/v1637431015/People_Surfing_cqmlmx.mp4', {
 
       public_id: `surf-${Date.now()}`,
       resource_type: 'video',
