0c0594ee80492a6bd89025a79d8b6decabe69fba
rebeccapeltz <rebeccapeltz@gmail.com>
2022-08-01 18:49:35 -0700

    turn on video player analytics

--- components/video-element.js
+++ components/video-element.js
@@ -1,11 +1,16 @@
 import React, { useRef } from 'react';
 import { AdvancedVideo } from '@cloudinary/react';
-import { CloudinaryVideo } from '@cloudinary/url-gen';
+import { URLConfig } from '@cloudinary/url-gen';
+import { CloudConfig } from '@cloudinary/url-gen';
+import { CloudinaryVideo} from '@cloudinary/url-gen';
 
 
 const CloudinaryVideoElement = (props) => {
+
   const videoEl = useRef();
-  const video = new CloudinaryVideo(props.publicId,{cloudName:props.cloudName,analytics:false});
+  const cloudConfig = new CloudConfig({ cloudName: props.cloudName });
+  const urlConfig = new URLConfig({ secure: true, analytics: false });
+  const video = new CloudinaryVideo(props.publicId, cloudConfig, urlConfig);
   return <AdvancedVideo cldVid={video} width='100%' ref={videoEl} controls />;
 };
 export default CloudinaryVideoElement;
